9fa3cae2d3b6df9a9ab0226db155f7df4e07db08
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-08 15:21:36 +0100

    renamed join to joinPath

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ gamePackageName=f'{packagePrefix}{name}.pak'
 import os
 import io
 import zipfile
-join=os.path.join
+joinPath=os.path.joinPath
 
 #######
 # PAK #
@@ -22,14 +22,15 @@ join=os.path.join
 gamePackage = io.BytesIO()
 zout = zipfile.ZipFile( gamePackage, 'w', compression=zipfile.ZIP_STORED )
 
-directory = join('Config', 'CVarGroups')
+directory = joinPath('Config', 'CVarGroups')
 for file in os.listdir(directory):
 	filename = os.fsdecode(file)
-	zout.write( join(directory, filename) )
+	zout.write( joinPath(directory, filename) )
 
-zout.write( join('Libs', 'UI', 'UIActions', 'MM_AdvancedGraphics.xml') )
-zout.write( join('Libs', 'UI', 'UIActions', 'MM_IngameMenu.xml') )
-zout.write( join('Libs', 'UI', 'UIActions', 'SYS_Main_1.xml') )
+zout.write( joinPath('Libs', 'UI', 'UIActions', 'MM_AdvancedGraphics.xml') )
+zout.write( joinPath('Libs', 'UI', 'UIActions', 'MM_IngameMenu.xml') )
+zout.write( joinPath('Libs', 'UI', 'UIActions', 'SYS_Main_1.xml') )
+zout.write( joinPath('Libs', 'FlowgraphModules', 'SYS_Main_1.xml') )
 
 zout.close()
 
@@ -37,28 +38,28 @@ zout.close()
 # ZIP #
 #######
 
-zout = zipfile.ZipFile( join(outputDir, zipFilename), 'w', compression=zipfile.ZIP_DEFLATED )
+zout = zipfile.ZipFile( joinPath(outputDir, zipFilename), 'w', compression=zipfile.ZIP_DEFLATED )
 
 zout.write('ogp_hotkeys.cfg')
 
 directory = 'Config'
 for file in os.listdir(directory):
 	filename = os.fsdecode(file)
-	if os.path.isfile(join(directory, filename) ):
-		zout.write( join(directory, filename) )
+	if os.path.isfile(joinPath(directory, filename) ):
+		zout.write( joinPath(directory, filename) )
 
 directory = 'optimized_graphic_presets'
 for file in os.listdir(directory):
 	filename = os.fsdecode(file)
-	zout.write( join(directory, filename) )
+	zout.write( joinPath(directory, filename) )
 
-zout.write( 'README.md', join(name, 'README.md') )
+zout.write( 'README.md', joinPath(name, 'README.md') )
 
 zout.write(
-	join('dep', 'zzzzzz_kcd_flowgraph_hook.pak'),
-	join('Data', 'zzzzzz_kcd_flowgraph_hook.pak')
+	joinPath('dep', 'zzzzzz_kcd_flowgraph_hook.pak'),
+	joinPath('Data', 'zzzzzz_kcd_flowgraph_hook.pak')
 )
 
-zout.writestr( join('Data', gamePackageName), gamePackage.getvalue() )
+zout.writestr( joinPath('Data', gamePackageName), gamePackage.getvalue() )
 
 zout.close()
