118ad93ae8f29bf1efe0d4b6b793ac3b3a1709c9
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-12 22:30:48 +0100

    PascalCase func renaming

--- mod/Data/core.pak/Scripts/ogp_string_util.lua
+++ mod/Data/core.pak/Scripts/ogp_string_util.lua
@@ -2,10 +2,10 @@
 
 ogp.string = {}
 
-function ogp.string.starts( str, pattern )
+function ogp.string.Starts( str, pattern )
    return string.sub( str, 1, string.len(pattern) ) == pattern
 end
 
-function ogp.string.ends( str, pattern )
+function ogp.string.Ends( str, pattern )
    return string.sub( str, -string.len(pattern) ) == pattern
 end
