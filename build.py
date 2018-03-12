ae7a066f77918e63f38a53d39adeb29d7fb6bcd7
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-12 06:10:52 +0100

    fixed build for new structure

--- build.py
+++ build.py
@@ -1,50 +1,37 @@
 #!/usr/bin/env python3.6
 
 version='3.0.0'
-name='optimized_graphic_presets'
+projectName='optimized_graphic_presets'
 packagePrefix='zzz_'
 outputDir='builds'
 
 #######################################
 
-zipFilename=f'{name}_{version}.zip'
-gamePackageName=f'{packagePrefix}{name}.pak'
+zipFilename=f'{projectName}_{version}.zip'
 
 import io
 import zipfile
 from pathlib import Path
 
-#######
-# PAK #
-#######
-
-gamePackage = io.BytesIO()
-zout = zipfile.ZipFile( gamePackage, 'w', compression=zipfile.ZIP_STORED )
-# add all files in pak folder
-for file in Path('pak').glob('**/*.*'):
-	zout.write( file, file.relative_to('pak') )
-zout.close()
-
-#######
-# ZIP #
-#######
-
 zout = zipfile.ZipFile( Path(outputDir, zipFilename), 'w', compression=zipfile.ZIP_DEFLATED )
 
-# hotkeys config
-zout.write('ogp_hotkeys.cfg')
-# presets
-for file in Path('Config').glob('*.cfg'):
-	zout.write(file)
-# add {name} folder
-for file in Path(name).glob('**/*'):
-	zout.write( file, Path(name, file.name) )
 # README
-zout.write( 'README.md', Path(f'{name}/README.md') )
-# Package dependencies
-for file in Path('dep').glob('*.pak'):
-	zout.write( file, Path('Data',file.name) )
-# New package
-zout.writestr( str(Path('Data', gamePackageName)), gamePackage.getvalue() )
+zout.write( 'README.md', Path(f'{projectName}/README.md') )
+
+# Files in mod (not in sub dirs)
+for file in Path('mod').glob('*.*'):
+	if file.is_file():
+		zout.write( str(file), Path(f'{projectName}/{file.name}') )
+
+
+# Create in-memory package for each folder with .pak folder in mod/Data
+for pak in Path('mod/Data').glob('**/*.pak'):
+	if pak.is_dir():
+		gamePackage = io.BytesIO()
+		pakage = zipfile.ZipFile( gamePackage, 'w', compression=zipfile.ZIP_STORED )
+		for file in pak.glob('**/*.*'):
+			pakage.write( file, file.relative_to(pak) )
+		pakage.close()
+		zout.writestr( str(Path(projectName, 'Data', pak.name)), gamePackage.getvalue() )
 
 zout.close()
