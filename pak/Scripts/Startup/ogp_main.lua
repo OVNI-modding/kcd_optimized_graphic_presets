6785f861503fdb880d486944bdaa8f0f60654ad3
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-12 03:15:30 +0100

    added lua code

---
+++ pak/Scripts/Startup/ogp_main.lua
@@ -0,0 +1,92 @@
+ogp = {}
+System.LogAlways( "[OGP] initializing..." )
+
+ogp.locals = {}
+ogp.locals.english = {}
+ogp.locals.english["Apply (without leaving)"] = "Apply (without leaving)"
+
+-- Action.SaveXML(const char * definitionFile, const char * dataFile, SmartScriptTable dataTable)
+-- Action.PauseGame( pause )
+
+---
+---
+---
+function ogp.Init()
+	Script.ReloadScript( "Scripts/ogp_menu_util.lua" )
+	local root = CryAction.LoadXML( "ogp_advancedSettingsMenu_def.xml", "ogp_advancedSettingsMenu.xml" )
+	ogp.buttons = root.buttons;
+	System.AddCCommand( "ogp_load_settings", "ogp.LoadSettings(%line)", "" )
+	System.AddCCommand( "ogp_create_advanced_settings_menu", "ogp.CreateAdvancedSettingsMenu()", "" )
+	System.AddCCommand( "ogp_create_advanced_settings_buttons", "ogp.CreateAdvancedSettingsButtons()", "" )
+	System.LogAlways( "[OGP] initialized" )
+end
+
+---
+--- Localizes given sting using ogp.locals[g_language] or ogp.locals.english
+---
+function ogp.localize( string )
+	local language = System.GetCVar("g_language")
+	if ogp.locals[language] == nil then language="english" end
+	return ogp.locals[language][string] or ogp.locals.english[string]
+end
+
+---
+---
+---
+function ogp.LoadSettings(xmlFile)
+
+end
+
+---
+---
+---
+function ogp.SaveSettings(xmlFile)
+
+end
+
+---
+---
+---
+function ogp.CreateAdvancedSettingsMenu()
+	ogp.menu_util.ClearAll()
+	ogp.menu_util.SetTitleBox( "controls" )
+	ogp.menu_util.SetContainer( 1500, 250, 13 )
+	ogp.CreateAdvancedSettingsButtons()
+	ogp.menu_util.AddButton( "apply", ogp.localize("Apply (without leaving)"), "", ogp.menu_util.actionTypes.apply, 1 )
+	ogp.menu_util.AddButton( "back", "@ui_back", "", ogp.menu_util.actionTypes.back, 1 )
+end
+
+---
+---
+---
+function ogp.CreateAdvancedSettingsButtons()
+	for _,button in ipairs(ogp.buttons) do
+		ogp.menu_util.AddChoiceButton( button.cvar, button.label, button.tooltip )
+		for _,choice in ipairs(button.choices) do
+			ogp.menu_util.AddChoiceOption( button.cvar, choice.label, choice.tooltip, choice.value )
+		end
+		local value = System.GetCVar(button.cvar)
+		if value < 0 then
+			value = System.GetCVar( button.cvar .. "_vanilla" )
+		end
+		ogp.menu_util.SetChoice( button.cvar, value )
+	end
+end
+
+---
+---
+---
+function ogp.ApplySettings()
+	for _,button in ipairs(ogp.buttons) do
+		local cvar = button.cvar
+		local choice = ogp.menu_util.GetChoice(cvar)
+		if choice > 10 then
+			cvar = cvar .. "_vanilla"
+			choice = choice - 10
+		end
+		System.SetCVar(cvar, value)
+	end
+end
+
+
+ogp.Init()
---
+++ pak/Scripts/ogp_menu_util.lua
@@ -0,0 +1,83 @@
+
+ogp.menu_util = {}
+
+---
+--- Action type for buttons
+---
+ogp.menu_util.actionTypes = { apply="apply", back="back", reset="reset" }
+
+---
+--- Action type for buttons
+---
+function ogp.menu_util.ClearAll()
+	local useLastSelect = 0
+	UIAction.CallFunction( "MainMenu", -1, "ClearAll", useLastSelect )
+end
+
+---
+--- Setup a new screen start
+---
+function ogp.menu_util.SetContainer( buttonXPos, buttonYPos, maxButtons )
+	local containerIndex = 0
+	UIAction.CallFunction( "MainMenu", -1, "SetContainer", containerIndex, buttonXPos, buttonYPos, maxButtons )
+end
+
+---
+---
+---
+function ogp.menu_util.SetTitleBox( header )
+	UIAction.CallFunction( "MainMenu", -1, "SetTitleBox", header )
+end
+
+---
+--- Darkens the menu 0/1
+---
+function ogp.menu_util.SetMenuColor(state)
+	UIAction.CallFunction( "MainMenu", -1, "SetMenuColor", tonumber(state) )
+end
+
+---
+--- Adds a simple button to the menu
+---
+function ogp.menu_util.AddButton( buttonId, label, tooltip, actionType, containerIndex )
+	local disabled = false
+	local sound = ""
+	UIAction.CallFunction( "MainMenu", -1, "AddButton", buttonId, containerIndex, label, actionType, tooltip, disabled, sound )
+end
+
+---
+---
+---
+function ogp.menu_util.AddChoiceButton( buttonId, label, tooltip )
+	local containerIndex = 0
+	local disabled = false
+	local sound = ""
+	System.LogAlways( "ogp.menu_util.AddChoiceButton" )
+	UIAction.CallFunction( "MainMenu", -1, "AddChoicesButton", buttonId, containerIndex, label, tooltip, disabled, sound )
+end
+
+---
+---
+---
+function ogp.menu_util.AddChoiceOption( buttonId, label, tooltip, value )
+	local containerIndex = 0
+	local disabled = false
+	local sound = ""
+	UIAction.CallFunction( "MainMenu", -1, "AddChoiceOption", value, buttonId, containerIndex, label, tooltip, disabled, sound )
+end
+
+---
+---
+---
+function ogp.menu_util.GetChoice( buttonId )
+	local containerIndex = 0
+	return tonumber( UIAction.CallFunction("MainMenu", -1, "GetChoice", buttonId, containerIndex) )
+end
+
+---
+---
+---
+function ogp.menu_util.SetChoice( buttonId, value )
+	local containerIndex = 0
+	UIAction.CallFunction( "MainMenu", -1, "SetChoice", buttonId, containerIndex, value )
+end
