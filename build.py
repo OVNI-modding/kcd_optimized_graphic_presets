fdef5dd50eb850109a744478d77aac4d511d2354
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-13 03:12:41 +0100

    back to old mod format (new one doesn't allow to create Cvargroups)

--- build.py
+++ build.py
@@ -13,6 +13,39 @@ import io
 import zipfile
 from pathlib import Path
 
+
+zout = zipfile.ZipFile( Path(outputDir, zipFilename), 'w', compression=zipfile.ZIP_DEFLATED )
+
+# README
+zout.write( 'README.md', Path(f'Mods/{projectName}/README.md') )
+
+
+# Create in-memory package for each folder with .pak folder in mod/Data
+for pak in Path('mod/Data').glob('**/*.pak'):
+	if pak.is_dir():
+		gamePackage = io.BytesIO()
+		pakage = zipfile.ZipFile( gamePackage, 'w', compression=zipfile.ZIP_STORED )
+		for file in pak.glob('**/*.*'):
+			pakage.write( file, file.relative_to(pak) )
+		pakage.close()
+		zout.writestr( str(Path('Data', packagePrefix+pak.name)), gamePackage.getvalue() )
+
+# Files in mod (not in sub dirs)
+for file in Path('mod').glob('*.*'):
+	if file.is_file():
+		zout.write( str(file), Path(f'Mods/{projectName}/{file.name}') )
+
+zout.close()
+
+
+
+
+
+# IMPORTANT
+#The new mods format doesn't allow to create cvargroup, and thus is totally uncompatible with this mod
+exit()
+
+
 zout = zipfile.ZipFile( Path(outputDir, zipFilename), 'w', compression=zipfile.ZIP_DEFLATED )
 
 # README
