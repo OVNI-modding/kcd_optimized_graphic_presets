0578348a9b79186941cdaf1565a0f9ef9f719b5d
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-13 19:25:50 +0100

    added profile change hotkeys

---
+++ mod/Data/hotkeys.pak/Scripts/Startup/ogp_hokeys.lua
@@ -0,0 +1,12 @@
+
+System.ExecuteCommand("bind f1 \"loadconfig ugly\"")
+System.ExecuteCommand("bind f2 \"loadconfig low\"")
+System.ExecuteCommand("bind f3 \"loadconfig medium\"")
+System.ExecuteCommand("bind f4 \"loadconfig high\"")
+System.ExecuteCommand("bind f5 \"loadconfig ultra\"")
+
+System.ExecuteCommand("bind f6 \"sys_spec_ogp_all_vanilla 1\"")
+System.ExecuteCommand("bind f7 \"sys_spec_ogp_all_vanilla 2\"")
+System.ExecuteCommand("bind f8 \"sys_spec_ogp_all_vanilla 3\"")
+System.ExecuteCommand("bind f9 \"sys_spec_ogp_all_vanilla 4\"")
+System.ExecuteCommand("bind f10 \"sys_spec_ogp_all_vanilla 7\"")
