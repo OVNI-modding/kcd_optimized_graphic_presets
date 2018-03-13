d8d44473c3f06eaf94f9fe0d9759afe4db5a6dbe
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-13 15:08:20 +0100

    no longer create Mods folder

--- build.py
+++ build.py
@@ -16,12 +16,12 @@ from pathlib import Path
 zout = zipfile.ZipFile( Path(outputDir, zipFilename), 'w', compression=zipfile.ZIP_DEFLATED )
 
 # README
-zout.write( 'README.md', Path(f'Mods/{projectName}/README.md') )
+zout.write( 'README.md', Path(f'{projectName}/README.md') )
 
 # Files in mod (not in sub dirs)
 for file in Path('mod').glob('*.*'):
 	if file.is_file():
-		zout.write( str(file), Path(f'Mods/{projectName}/{file.name}') )
+		zout.write( str(file), Path(f'{projectName}/{file.name}') )
 
 
 # Create in-memory package for each folder with .pak folder in mod/Data
@@ -32,6 +32,6 @@ for pak in Path('mod/Data').glob('**/*.pak'):
 		for file in pak.glob('**/*.*'):
 			pakage.write( file, file.relative_to(pak) )
 		pakage.close()
-		zout.writestr( str(Path('Mods', projectName, 'Data', packagePrefix+pak.name)), gamePackage.getvalue() )
+		zout.writestr( str(Path(projectName, 'Data', packagePrefix+pak.name)), gamePackage.getvalue() )
 
 zout.close()
