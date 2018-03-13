1321a899608ead76b2ee5765cfaf591778d954e1
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-13 16:15:31 +0100

    make SetOgpVar callable from console/flowgraph

--- mod/Data/optimized_graphic_presets.pak/Scripts/Startup/ogp_main.lua
+++ mod/Data/optimized_graphic_presets.pak/Scripts/Startup/ogp_main.lua
@@ -42,6 +42,7 @@ function ogp.AddConsoleCommands()
 	-- System.AddCCommand( "ogp_load_settings", "ogp.LoadSettings()", "" )
 	System.AddCCommand( "ogp_apply_settings", "ogp.ApplySettings()", "" )
 	System.AddCCommand( "ogp_save_settings", "ogp.SaveSettings()", "" )
+	System.AddCCommand( "ogp_set_cvar", "ogp.SetCVar(%1, %2)", "" )
 	System.LogAlways( "[OGP] initialized" )
 end
 
@@ -99,6 +100,7 @@ end
 --- Should not be used directly, use SetCVar instead.
 ---
 function ogp.SetOgpCVar( cvar, value )
+	value = tonumber(value)
 	if value > 0 then
 		cvar = cvar .. "_vanilla"
 	end
