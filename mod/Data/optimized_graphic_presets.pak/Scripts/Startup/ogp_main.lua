782c2e532dcfd70afea37be82c7f1e9f43584c10
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-14 20:31:32 +0100

    else if => elseif

--- mod/Data/optimized_graphic_presets.pak/Scripts/Startup/ogp_main.lua
+++ mod/Data/optimized_graphic_presets.pak/Scripts/Startup/ogp_main.lua
@@ -28,35 +28,33 @@ function ogp.AddConsoleCommands()
 	System.AddCCommand( "ogp_set_profile", "ogp.SetProfile(%1)", "" )
 end
 
-
 ---
 --- Sets all-in-on graphic profile : -1 to -5 for ogp, 1,2,3,4,7 for vanilla
 ---
 function ogp.SetProfile( value )
 	if value == -1 then
 		System.ExecuteCommand( "loadconfig ogp_ugly" )
-	else if value == -2 then
+	elseif value == -2 then
 		System.ExecuteCommand( "loadconfig ogp_low" )
-	else if value == -3 then
+	elseif value == -3 then
 		System.ExecuteCommand( "loadconfig ogp_medium" )
-	else if value == -4 then
+	elseif value == -4 then
 		System.ExecuteCommand( "loadconfig ogp_high" )
-	else if value == -5 then
+	elseif value == -5 then
 		System.ExecuteCommand( "loadconfig ogp_ultra" )
-	else if value == 1 then
+	elseif value == 1 then
 		System.ExecuteCommand( "loadconfig ogp_vanilla_low" )
-	else if value == 2 then
+	elseif value == 2 then
 		System.ExecuteCommand( "loadconfig ogp_vanilla_medium" )
-	else if value == 3 then
+	elseif value == 3 then
 		System.ExecuteCommand( "loadconfig ogp_vanilla_high" )
-	else if value == 4 then
+	elseif value == 4 then
 		System.ExecuteCommand( "loadconfig ogp_vanilla_veryhigh" )
-	else if value == 7 then
+	elseif value == 7 then
 		System.ExecuteCommand( "loadconfig ogp_vanilla_ultra" )
 	else
 		ogp.LogError("in SetProfile, unknown value:" .. value)
 	end
 end
-
 
 ogp.Init()
