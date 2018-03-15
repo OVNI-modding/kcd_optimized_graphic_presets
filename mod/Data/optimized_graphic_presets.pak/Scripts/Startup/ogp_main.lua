e7dc53c96106d27859561e5b1bb2e5d925fe29bb
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-15 23:02:14 +0100

    re-enabled ogp_load_settings console command

--- mod/Data/optimized_graphic_presets.pak/Scripts/Startup/ogp_main.lua
+++ mod/Data/optimized_graphic_presets.pak/Scripts/Startup/ogp_main.lua
@@ -21,7 +21,7 @@ end
 ---
 function ogp.AddConsoleCommands()
 	System.AddCCommand( "ogp_create_advanced_settings_buttons", "ogp.CreateAdvancedSettingsButtons()", "" )
-	-- System.AddCCommand( "ogp_load_settings", "ogp.LoadSettings()", "" )
+	System.AddCCommand( "ogp_load_settings", "ogp.LoadSettings()", "" )
 	System.AddCCommand( "ogp_apply_settings", "ogp.ApplySettings()", "" )
 	System.AddCCommand( "ogp_save_settings", "ogp.SaveSettings()", "" )
 	System.AddCCommand( "ogp_set_profile", "ogp.SetProfile(%1)", "" )
