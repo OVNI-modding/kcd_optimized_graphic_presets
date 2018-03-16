bf7513231e0e1f228ae5f426698ac736574c950f
OVNI_modding <7042209+OVNI-modding@users.noreply.github.com>
2018-03-16 06:50:09 +0100

    added ogp_cvar_util test suite

---
+++ mod/Data/core.pak/Scripts/tests/ogp_cvar_util.lua
@@ -0,0 +1,8 @@
+if ogp.loadedFiles['tests/ogp_cvar_util.lua']==nil then ogp.loadedFiles['tests/ogp_cvar_util.lua']=1
+
+
+Script.ReloadScript( "Scripts/tests/ogp_cvar_util_GetCVar.lua" )
+Script.ReloadScript( "Scripts/tests/ogp_cvar_util_SetCVar.lua" )
+
+
+end
